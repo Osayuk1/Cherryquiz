33e160225f397cdcc7c7ab794e7b08442f4f0e6e
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2024-10-14 16:51:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ document.getElementById("quizForm").addEventListener("submit", function(e) {
     const webhookUrl = "https://discord.com/api/webhooks/1268893038793719859/_ktjZVX-uHx8UVoYu7GAdrpkRLpbysF11nl120aBoWKRwdsY06g_9dAq1HYG7yeWvqwk";
     
     const answers = {
+        discordName: document.getElementById("discordName").value,
+        tradeName: document.getElementById("tradeName").value,
         question1: document.getElementById("question1").value,
         question2: document.getElementById("question2").value,
         question3: document.getElementById("question3").value
     };
 
     const payload = {
-        content: `Quiz Answers:\n1. ${answers.question1}\n2. ${answers.question2}\n3. ${answers.question3}`
+        content: `Discord Name: ${answers.discordName}\nTrade Name: ${answers.tradeName}\n\nQuiz Answers:\n1. ${answers.question1}\n2. ${answers.question2}\n3. ${answers.question3}`
     };
 
     fetch(webhookUrl, {
